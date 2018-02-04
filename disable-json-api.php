8421f9607daf9570a42ff9a43e7a29922d60fc22
Dave McHale <dmchale@gmail.com>
2018-02-04 12:36:28 -0500

    Version tag to 1.4.3-beta

--- disable-json-api.php
+++ disable-json-api.php
@@ -3,7 +3,7 @@
  * Plugin Name: Disable REST API
  * Plugin URI: http://www.binarytemplar.com/disable-json-api
  * Description: Disable the use of the JSON REST API on your website to anonymous users
- * Version: 1.4.2
+ * Version: 1.4.3-beta
  * Author: Dave McHale
  * Author URI: http://www.binarytemplar.com
  * Text Domain: disable-json-api
