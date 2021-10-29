196d68bc5163955ed51ddf070c82aa12ee8f5f05
Dave McHale <dmchale@users.noreply.github.com>
2021-10-29 16:25:23 -0400

    Revert "Merge pull request #31 from skaudallas/accordion"
    This reverts commit f875090f2ea30235e34252b65c2513548d7e1747.

--- classes/admin.php
+++ classes/admin.php
@@ -21,14 +21,12 @@ class DRA_Admin {
 
 			if ( $is_route_namespace || "/" == $route ) {
 				$current_namespace = $route;
-				$block_class = ( "/" == $route ) ? "block-style" : "accordion-container";
-				$accordion_button = ( "/" == $route ) ? '' : "<button type='button' class='accordion-expand'></button>";
-
 				if ( 0 != $loopCounter ) {
-					echo "</ul></div>";
+					echo "</ul>";
 				}
+
 				$route_for_display = ( "/" == $route ) ? "/ <em>" . esc_html__( "REST API ROOT", "disable-json-api" ) . "</em>" : esc_html( $route );
-				echo "<div class='". $block_class . "'><div class='accordion-top'><div class='accordion-label'><label class='switch'><input name='rest_routes[]' value='$route' type='checkbox' id='dra_namespace_$loopCounter' onclick='dra_namespace_click(\"$route\", $loopCounter)' $checkedProp><span class='slider'></span></label><h2><label for='dra_namespace_$loopCounter'>&nbsp;$route_for_display</label></h2></div>" . $accordion_button . "</div><ul>";
+				echo "<label class='switch'><input name='rest_routes[]' value='$route' type='checkbox' id='dra_namespace_$loopCounter' onclick='dra_namespace_click(\"$route\", $loopCounter)' $checkedProp><span class='slider'></span></label><h2><label for='dra_namespace_$loopCounter'>&nbsp;$route_for_display</label></h2><ul>";
 
 				if ( "/" == $route ) {
 					echo "<li>" . sprintf( esc_html__( "On this website, the REST API root is %s", "disable-json-api" ), "<strong>" . rest_url() . "</strong>" ) . "</li>";
@@ -40,7 +38,7 @@ class DRA_Admin {
 
 			$loopCounter ++;
 		}
-		echo "</ul></div>";
+		echo "</ul>";
 	}
 
 
