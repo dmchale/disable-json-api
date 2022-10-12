b82438a4dfffdb03e3d1276977a7bb661593d0e9
Dave McHale <dmchale@gmail.com>
2022-10-12 16:50:38 -0400

    Using PHPStorm to enforce WP Code Style on php files

--- admin.php
+++ admin.php
@@ -7,37 +7,37 @@
         <strong><?php esc_html_e( "IMPORTANT NOTE:", "disable-json-api" ); ?></strong> <?php esc_html_e( "Checking a box merely restores default functionality to an endpoint. Other authentication and/or permissions may still be required for access, or other themes/plugins may also affect access to those endpoints.", "disable-json-api" ); ?>
     </p>
 
-    <hr />
+    <hr/>
 
     <div id="select-container">
         <label for="dra-role"><?php esc_html_e( "Rules for", "disable-json-api" ); ?>:</label> <select name="role" id="dra-role">
             <option value="none"><?php esc_html_e( "Unauthenticated Users", "disable-json-api" ); ?></option>
-            <?php
-            $role = ( isset( $_GET['role'] ) ) ? $_GET['role'] : 'none';
-            wp_dropdown_roles( $role );
-            ?>
+			<?php
+			$role = ( isset( $_GET['role'] ) ) ? $_GET['role'] : 'none';
+			wp_dropdown_roles( $role );
+			?>
         </select>
     </div>
 
-    <hr />
+    <hr/>
 
     <form method="post" action="" id="DRA_form">
-	    <?php wp_nonce_field( 'DRA_admin_nonce' ); ?>
+		<?php wp_nonce_field( 'DRA_admin_nonce' ); ?>
         <input type="hidden" name="role" value="<?php echo esc_attr( $role ); ?>">
 
         <div id="default-allow-container">
-            <?php DRA_Admin::display_role_default_allow( $role ); ?>
+			<?php DRA_Admin::display_role_default_allow( $role ); ?>
         </div>
 
-        <hr />
+        <hr/>
 
         <div id="route-container">
-            <?php DRA_Admin::display_route_checkboxes( $role ); ?>
-            <hr />
+			<?php DRA_Admin::display_route_checkboxes( $role ); ?>
+            <hr/>
         </div>
 
         <div id="button-container">
-            <?php submit_button(); ?>
+			<?php submit_button(); ?>
             <input type="submit" name="reset" id="dra-reset-button" class="button button-secondary"
                    value="<?php esc_attr_e( "Reset Allowed List of Routes", "disable-json-api" ); ?>"
                    onclick="return confirm('<?php esc_attr_e( "Are you sure you wish to reset all allowed routes for this user role?", "disable-json-api" ); ?>');">
--- classes/disable-rest-api.php
+++ classes/disable-rest-api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Disable_REST_API class
  *
@@ -72,7 +73,7 @@ class Disable_REST_API {
 	 * @return string
 	 */
 	private function get_current_route() {
-		$rest_route = isset($GLOBALS['wp']->query_vars['rest_route']) ?
+		$rest_route = isset( $GLOBALS['wp']->query_vars['rest_route'] ) ?
 			$GLOBALS['wp']->query_vars['rest_route'] :
 			'';
 
@@ -91,17 +92,17 @@ class Disable_REST_API {
 	 */
 	private function is_route_allowed( $currentRoute ) {
 
-		$current_options = get_option( 'disable_rest_api_options', array() );
+		$current_options    = get_option( 'disable_rest_api_options', array() );
 		$current_user_roles = $this->get_current_user_roles();
 
 		// Loop through user roles belonging to the current user
 		foreach ( $current_user_roles as $role ) {
 
 			// If we have a definition for the current user's role
-			if ( isset( $current_options['roles'][$role] ) ) {
+			if ( isset( $current_options['roles'][ $role ] ) ) {
 
 				// If any role for this user is set to Allow Full REST API Access, return true automatically
-				if ( true === $current_options['roles'][$role]['default_allow'] ) {
+				if ( true === $current_options['roles'][ $role ]['default_allow'] ) {
 					return true;
 				}
 
@@ -216,6 +217,7 @@ class Disable_REST_API {
 		$role = ( isset( $_POST['role'] ) ) ? $_POST['role'] : 'dra-undefined';
 		if ( ! DRA_Helpers::is_valid_role( $role ) ) {
 			add_settings_error( 'DRA-notices', esc_attr( 'settings_updated' ), esc_html__( 'Invalid user role detected when processing form. No updates have been made.', 'disable-json-api' ), 'error' );
+
 			return;
 		}
 
@@ -233,20 +235,20 @@ class Disable_REST_API {
 
 			// Unauthorized users default to no routes allowed. All other user roles default to allowing all routes
 			$rest_routes_for_setting = DRA_Helpers::build_routes_rule_for_all( $default_allow );
-			$msg = esc_html__( 'All allowlists have been reset for this user role.', 'disable-json-api' );
+			$msg                     = esc_html__( 'All allowlists have been reset for this user role.', 'disable-json-api' );
 
 		} else {
 
 			// Get back the full list of true/false routes based on the posted routes allowed
 			$rest_routes_for_setting = DRA_Helpers::build_routes_rule( $rest_routes );
-			$msg = esc_html__( 'Allowlist settings saved for this user role.', 'disable-json-api' );
+			$msg                     = esc_html__( 'Allowlist settings saved for this user role.', 'disable-json-api' );
 
 		}
 
 		// Save only the rules for this role back to itself
-		$arr_option['roles'][$role] = array(
-			'default_allow'     => $default_allow,
-			'allow_list'        => $rest_routes_for_setting,
+		$arr_option['roles'][ $role ] = array(
+			'default_allow' => $default_allow,
+			'allow_list'    => $rest_routes_for_setting,
 		);
 
 		// Save allowlist to the Options table and return with message for user
@@ -275,8 +277,8 @@ class Disable_REST_API {
 	 * @return WP_Error
 	 */
 	private function get_wp_error( $access ) {
-        $dra_error_message = apply_filters( 'dra_error_message', 'DRA: Only authenticated users can access the REST API.', $access );
-		$error_message = esc_html__( $dra_error_message, 'disable-json-api' );
+		$dra_error_message = apply_filters( 'dra_error_message', 'DRA: Only authenticated users can access the REST API.', $access );
+		$error_message     = esc_html__( $dra_error_message, 'disable-json-api' );
 
 		if ( is_wp_error( $access ) ) {
 			$access->add( 'rest_cannot_access', $error_message, array( 'status' => rest_authorization_required_code() ) );
@@ -294,7 +296,7 @@ class Disable_REST_API {
 	public function option_check() {
 
 		// If our new option already exists, we can bail
-		if ( get_option( 'disable_rest_api_options') ) {
+		if ( get_option( 'disable_rest_api_options' ) ) {
 			return;
 		}
 
@@ -311,9 +313,9 @@ class Disable_REST_API {
 
 		// Define the basic structure of our new option
 		$arr_option = array(
-			'version'           => self::VERSION,       // the current version of this plugin
-			'default_allow'     => true,                // if a role is not specifically defined in the settings, should the default be to ALLOW the route or not?
-			'roles'             => array(),             // array of the user roles in this install of wordpress
+			'version'       => self::VERSION,       // the current version of this plugin
+			'default_allow' => true,                // if a role is not specifically defined in the settings, should the default be to ALLOW the route or not?
+			'roles'         => array(),             // array of the user roles in this install of wordpress
 		);
 
 		// Default list of allowed routes. By default, nothing is allowed because we're checking for our pre-v1.6 option here for migration purposes
@@ -327,8 +329,8 @@ class Disable_REST_API {
 
 		// Define the "unauthenticated" rules based on the old option value (or default value of "nothing")
 		$arr_option['roles']['none'] = array(
-			'default_allow'     => false,
-			'allow_list'        => $new_unauthenticated_rules,
+			'default_allow' => false,
+			'allow_list'    => $new_unauthenticated_rules,
 		);
 
 		// Save new option
@@ -355,6 +357,7 @@ class Disable_REST_API {
 		}
 
 		$user = wp_get_current_user();
+
 		return ( array ) $user->roles;
 
 	}
